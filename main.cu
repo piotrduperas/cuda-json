d5dd72bfc86f349255249e78467ee601b17606a6
Piotr Duperas <piotrduperas@gmail.com>
2021-04-11 03:38:01 +0200

    Added comments

--- main.cu
+++ main.cu
@@ -66,6 +66,8 @@ int main(int argc, char **argv)
 {
   cout << "CUDA JSON Validator" << endl << endl;
 
+
+  // Reading file
   const string& json_file = argc == 2 ? argv[1] : "samples/senators_short.json";
   ifstream ifile(json_file);
 
@@ -73,27 +75,37 @@ int main(int argc, char **argv)
   ss << ifile.rdbuf();
   const string& s = ss.str();
 
+
+
   thrust::host_vector<char> H_file(s.begin(), s.end());
+  thrust::device_vector<char> D_file(file_size);
+
   size_t file_size = H_file.size();
 
-  thrust::device_vector<char> D_file(file_size);
-  thrust::device_vector<char_and_position> D_braces_pos(file_size);
-
+  // Copy file to device memory
   thrust::copy(H_file.begin(), H_file.end(), D_file.begin());
 
+  // Vector for (brace, it's position) pair
+  thrust::device_vector<char_and_position> D_braces_pos(file_size);
+
+  // Zip file characters with their positions
   auto char_and_pos = thrust::make_zip_iterator(thrust::make_tuple(D_file.begin(), thrust::make_counting_iterator(0)));
   
+  // Filter only braces
   auto last_brace_it = thrust::copy_if(char_and_pos, char_and_pos + s.length(), D_braces_pos.begin(), is_brace());
   D_braces_pos.resize(last_brace_it - D_braces_pos.begin());
 
 
+  // Transform { to 1, } to -1
   thrust::transform(D_braces_pos.begin(), D_braces_pos.end(), D_braces_pos.begin(), braces_to_numbers());
 
   thrust::device_vector<short> D_levels(D_braces_pos.size());
 
+  // Calculate nesting levels of braces
   thrust::transform_inclusive_scan(D_braces_pos.begin(), D_braces_pos.end(), D_levels.begin(), get_char_from_tuple(), thrust::plus<short>());
   thrust::transform_if(D_levels.begin(), D_levels.end(), D_braces_pos.begin(), D_levels.begin(), increment(), is_closing_brace());
 
+  
   for(int i = 0; i < D_braces_pos.size(); i++){
     cout << thrust::get<1>((char_and_position)D_braces_pos[i]) << " - " << (thrust::get<0>((char_and_position)D_braces_pos[i]) == 1 ? '{' : '}') << " - " << D_levels[i] << endl;
   }

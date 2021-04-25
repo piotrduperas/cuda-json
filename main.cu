1890d7180c18675901e42ab2ba24130650de7d0a
Piotr Duperas <piotrduperas@gmail.com>
2021-04-25 19:07:00 +0200

    Time measurements

--- main.cu
+++ main.cu
@@ -14,6 +14,7 @@
 #include <algorithm>
 #include <iterator>
 #include <sstream>
+#include <chrono>
 
 using namespace std;
 
@@ -62,10 +63,19 @@ struct is_closing_brace
   }
 };
 
+void elapsedTime(chrono::time_point<chrono::steady_clock> start, string description) {
+  auto end = std::chrono::steady_clock::now();
+
+  cout << description << ": "
+    << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+    << "ms" << endl;
+}
+
 int main(int argc, char **argv)
 {
   cout << "CUDA JSON Validator" << endl << endl;
 
+  auto startReadingFile = chrono::steady_clock::now();
 
   // Reading file
   const string& json_file = argc == 2 ? argv[1] : "samples/senators_short.json";
@@ -75,7 +85,8 @@ int main(int argc, char **argv)
   ss << ifile.rdbuf();
   const string& s = ss.str();
 
-
+  elapsedTime(startReadingFile, "Reading file");
+  auto startCopying = chrono::steady_clock::now();
 
   thrust::host_vector<char> H_file(s.begin(), s.end());
   thrust::device_vector<char> D_file(H_file.size());
@@ -84,6 +95,9 @@ int main(int argc, char **argv)
 
   // Copy file to device memory
   thrust::copy(H_file.begin(), H_file.end(), D_file.begin());
+
+  elapsedTime(startCopying, "Copying file");
+  auto startCalculations = chrono::steady_clock::now();
 
   // Vector for (brace, it's position) pair
   thrust::device_vector<char_and_position> D_braces_pos(file_size);
@@ -105,10 +119,12 @@ int main(int argc, char **argv)
   thrust::transform_inclusive_scan(D_braces_pos.begin(), D_braces_pos.end(), D_levels.begin(), get_char_from_tuple(), thrust::plus<short>());
   thrust::transform_if(D_levels.begin(), D_levels.end(), D_braces_pos.begin(), D_levels.begin(), increment(), is_closing_brace());
 
+  elapsedTime(startCalculations, "Calculations");
+
   
-  for(int i = 0; i < D_braces_pos.size(); i++){
+  /*for(int i = 0; i < D_braces_pos.size(); i++){
     cout << thrust::get<1>((char_and_position)D_braces_pos[i]) << " - " << (thrust::get<0>((char_and_position)D_braces_pos[i]) == 1 ? '{' : '}') << " - " << D_levels[i] << endl;
-  }
+  }*/
   
 
   return 0;
